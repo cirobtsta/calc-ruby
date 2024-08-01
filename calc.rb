c042f5df8f3ade0acf47a0341f6ca83e4e1c2575
Ciro Batista <cirobtsta@gmail.com>
2024-08-01 06:19:36 -0300

    calc

--- calc.rb
+++ calc.rb
@@ -10,18 +10,57 @@ puts '3 - MULTIPLICAÇÃO'
 puts '4 - DIVISÃO'
 puts '0 - SAIR'
 
-op = gets.chomp
+op = gets.chomp.to_i
 
-puts 'DIGITE O PRIMEIRO NUMERO'
+if op == 1
 
-num1 = gets.chomp
+  puts 'DIGITE O PRIMEIRO NUMERO'
+  num1 = gets.chomp.to_i
+  puts 'DIGITE O SEGUNDO NUMERO'
+  num2 = gets.chomp.to_i
+  sum = (num1 + num2)
+  puts "= #{sum}"
 
-puts 'DIGITE O SEGUNDO NUMERO'
+elsif op == 2
 
-num2 = gets.chomp
+  puts 'DIGITE O PRIMEIRO NUMERO'
+  num1 = gets.chomp.to_i
+  puts 'DIGITE O SEGUNDO NUMERO'
+  num2 = gets.chomp.to_i
+  sub = (num1 - num2)
+  puts "= #{sub}"
+
+elsif op == 3
+
+  puts 'DIGITE O PRIMEIRO NUMERO'
+  num1 = gets.chomp.to_i
+  puts 'DIGITE O SEGUNDO NUMERO'
+  num2 = gets.chomp.to_i
+  mult = (num1 * num2)
+  puts "= #{mult}"
+
+elsif op == 4
+
+  puts 'DIGITE O PRIMEIRO NUMERO'
+  num1 = gets.chomp.to_i
+  puts 'DIGITE O SEGUNDO NUMERO'
+  num2 = gets.chomp.to_i
+  div = (num1 / num2)
+  puts "= #{div}"
+
+elsif op == 0
+  
+  break if op == 0
+
+else 
+  puts "OPCAO INVALIDA"  
+
+ 
+end
+
+system "clear"
+  
 
 
-soma = (num1 + num2)
 
-puts soma
 
