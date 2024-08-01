03ebe89dc770b553dba337a14edf9d2235f37828
Ciro Batista <cirobtsta@gmail.com>
2024-08-01 05:53:38 -0300

    calc

---
+++ calc.rb
@@ -0,0 +1,27 @@
+# MENU
+
+
+puts "Escolha uma opção DIGITE: "
+
+
+puts '1 - SOMA' 
+puts '2 - SUBTRAÇÃO'
+puts '3 - MULTIPLICAÇÃO'
+puts '4 - DIVISÃO'
+puts '0 - SAIR'
+
+op = gets.chomp
+
+puts 'DIGITE O PRIMEIRO NUMERO'
+
+num1 = gets.chomp
+
+puts 'DIGITE O SEGUNDO NUMERO'
+
+num2 = gets.chomp
+
+
+soma = (num1 + num2)
+
+puts soma
+
